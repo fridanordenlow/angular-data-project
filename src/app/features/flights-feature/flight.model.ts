93de30a0c40c10f36d75f28b4655b39599f34179
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-11-04 16:54:47 +0100

    update flight models

--- src/app/features/flights-feature/flight.model.ts
+++ src/app/features/flights-feature/flight.model.ts
@@ -1,9 +1,9 @@
 export interface FlightData {
   airline_name: string;
-  arrival_airport: string;
-  arrival_city: string;
   departure_airport: string;
   departure_city: string;
+  arrival_airport: string;
+  arrival_city: string;
   flight_duration_hours: number;
   flight_number: string;
 }
@@ -17,3 +17,13 @@ export interface HeaderItems {
   label: string;
   sortField: keyof FlightData;
 }
+
+export interface FlightFilters {
+  airline_name: string;
+  arrival_airport: string;
+  arrival_city: string;
+  departure_airport: string;
+  departure_city: string;
+}
+
+export type FlightFilterKey = keyof FlightFilters;
